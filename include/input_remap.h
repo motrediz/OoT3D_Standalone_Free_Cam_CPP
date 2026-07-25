22f6afe8da03e623bbad2d63a7dfaf51510e8d62
tristangnl <tristan.gnl@gmail.com>
2026-07-25 13:04:36 +0200

    remove unused proto

--- include/input_remap.h
+++ include/input_remap.h
@@ -5,6 +5,3 @@
 
 // Routes physical buttons to camera settings and native OoT3D actions.
 void InputRemap_Update(GlobalContext* globalCtx);
-
-// Sends a shortcut through the game's existing touchscreen handler by simulating a touch input on the bottom screen.
-void InputRemap_ApplyVanillaAction(ControlAction action);
