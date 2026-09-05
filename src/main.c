0c47f5b618ef7e4245ef9c855dcf173b1cd160d8
motrediz <161035581+motrediz@users.noreply.github.com>
2026-09-05 20:00:44 +0200

    Run ZR Fast Move in post-update hook

--- src/main.c
+++ src/main.c
@@ -35,5 +35,7 @@ void before_GlobalContext_Update(GlobalContext* globalCtx) {
 }
 
 void after_GlobalContext_Update(GlobalContext* globalCtx) {
+    // Apply ZR Fast Move after OoT3D has finished updating Link's movement.
+    InputRemap_AfterUpdate(globalCtx);
     displayHUD();
 }
